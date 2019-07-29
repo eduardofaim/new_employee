7438af33cc4a524028ab07a33f44a73f1443e750
Edu Faim <eduardofaim@gmail.com>
2019-07-29 00:03:24 -0300

    create employee working

--- features/specifications/employee.feature
+++ features/specifications/employee.feature
@@ -7,5 +7,5 @@ In order to manipulate startup registration information
 
 Scenario: Create an Employee
     Given API Address to Maintain Employee Registration
-    When making a request to register a employee
+    When making a request to register an employee
     Then the API will return the Startup registration data by response code 201
--- features/step_definitions/employee_steps.rb
+++ features/step_definitions/employee_steps.rb
@@ -1,8 +1,8 @@
 Given("API Address to Maintain Employee Registration") do
-    @uri_base = "http://dummy.restapiexample.com/create"
+    @uri_base = "http://dummy.restapiexample.com/api/v1/create"
   end
   
-  When("making a request to register a employee") do
+  When("making a request to register an employee") do
     @response = HTTParty.post(@uri_base, :body => {"name":"faim test 4","salary":"123","age":"35"}.to_json, :headers => {"Content-Type" => 'application/json'})
   end
   
